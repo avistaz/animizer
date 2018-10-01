370ccdb47e5a5445b6b4c906f041bd3efb285180
abixalmon <abixalmon@gmail.com>
2018-10-01 20:16:29 +0600

    refactored anime type

--- demo/anime.php
+++ demo/anime.php
@@ -1,9 +1,18 @@
 <?php
 
-require __DIR__.'/../vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
-$client = new \Animizer\Clients\AnidbClient('YOURAPIKEY');
-dump($client->get(0, 'anidb.xml'));
+//$client = new \Animizer\Clients\AnidbClient('YOURAPIKEY');
+//$anime = $client->get(0, 'anidb.xml');
+//dump($anime);
+//
+//$client = new \Animizer\Clients\AnnClient();
+//$anime = $client->get(0, 'ann.xml');
+//dump($anime);
 
-$client = new \Animizer\Clients\AnnClient();
-dump($client->get(0, 'ann.xml'));
+$client = new \Animizer\Clients\AnilistClient();
+$anime = $client->get(['mal' => 91941]);
+dump($anime);
+
+// https://kitsu.docs.apiary.io
+// http://anilist.co
